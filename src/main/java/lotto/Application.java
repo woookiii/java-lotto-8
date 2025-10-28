a10221db37bdc79b11f9482f39dd289b80d8b6f4
jungwook <mr4454@naver.com>
2025-10-28 22:35:11 +0900

    feat(guideCostInput): implement guideCostInput
    Implement guideCostInput:
    - print guide cost input

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        guideCostInput();
+    }
+
+    private static void guideCostInput() {
+        System.out.println("구입금액을 입력해 주세요.");
     }
 }
