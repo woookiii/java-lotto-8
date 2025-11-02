7417bc7de952d97dce485759d42f368d638547c0
jungwook <mr4454@naver.com>
2025-11-02 12:57:05 +0900

    test(CostValidator): add unit tests for isCostNotTooBig
    Add unit tests for isCostNotTooBig:
    - not throw error when valid lenght cost
    - throw error when invalid length cost
    - given when then format to all tests

--- src/test/java/lotto/validation/CostValidatorTest.java
+++ src/test/java/lotto/validation/CostValidatorTest.java
@@ -16,24 +16,50 @@ class CostValidatorTest {
 
     @DisplayName("1000원으로_나누어_떨어지는_가격이_들어왔을_때_에러를_던지지_않는다")
     @Test
-    void isCostLottoPriceTimes_validCost_doesNotThrow() {
-        final Long cost = 3000L;
+    void isCostLottoPriceTimes_valid() {
         final LottoNumber price = LottoNumber.PRICE;
         final ErrorMessage errorMessage = ErrorMessage.NOT_THOUSAND_TIMES_NUMBERS;
+
+        final Long cost = 3000L;
 
         assertDoesNotThrow(() -> validator.isCostLottoPriceTimes(cost, errorMessage, price));
     }
 
     @DisplayName("1000원으로_나누어_떨어지지_않는_가격이_들어왔을_때_에러를_메시지와_함께_던진다")
     @Test
-    void isCostLottoPriceTimes_invalidCost_throwsException() {
-        final Long cost = 3500L;
+    void isCostLottoPriceTimes_invalid() {
         final LottoNumber price = LottoNumber.PRICE;
         final ErrorMessage errorMessage = ErrorMessage.NOT_THOUSAND_TIMES_NUMBERS;
+
+        final Long cost = 3500L;
 
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
                 () -> validator.isCostLottoPriceTimes(cost, errorMessage, price)
+        );
+        assertEquals(errorMessage.getMessage(), exception.getMessage());
+    }
+
+    @DisplayName("정해둔_길이_미만의_수를_입력할_시_에러를_던지지_않는다")
+    @Test
+    void isCostNotTooBig_valid() {
+        final ErrorMessage errorMessage = ErrorMessage.TOO_BIG_COST;
+
+        final String validInput = "999999000";
+
+        assertDoesNotThrow(() -> validator.isCostNotTooBig(validInput, errorMessage));
+    }
+
+    @DisplayName("정해둔_길이_이상의_수를_입력할_시_에러를_메시지와_함께_던진다")
+    @Test
+    void isCostNotTooBig_invalid() {
+        final ErrorMessage errorMessage = ErrorMessage.TOO_BIG_COST;
+
+        final String invalidInput = "1234567890";
+
+        final IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> validator.isCostNotTooBig(invalidInput, errorMessage)
         );
         assertEquals(errorMessage.getMessage(), exception.getMessage());
     }
